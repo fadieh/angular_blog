f6e9ef979fa47e19c4a4f088dec9c34073f792c4
Fadie Hannona <fadiehannona@gmail.com>
2015-03-11 00:05:24 +0000

    routed home

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
-//  defining app routes. eg. how to hande a request like /home
-//  to define routes, need an angularJS module called ngRoute
-//  to use ngRoute, need to inject it or add into our app
 'use strict';
-
+ 
 angular.module('myApp', [
-	'ngRoute'
+    'ngRoute',
+    'myApp.home'           // Newly added home module
 ]).
 config(['$routeProvider', function($routeProvider) {
-	// routes will be here
-}])
+    // Set defualt view of our app to home
+     
+    $routeProvider.otherwise({
+        redirectTo: '/home'
+    });
+}]);
