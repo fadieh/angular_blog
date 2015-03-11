36b6a3204734b3ef526f6a37e34cc9651d83476a
Fadie Hannona <fadiehannona@gmail.com>
2015-03-11 04:01:43 +0000

    implemented signup functionality

--- app/register/register.js
+++ app/register/register.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.register', ['ngRoute'])
+angular.module('myApp.register', ['ngRoute', 'firebase'])
 
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/register', {
@@ -9,6 +9,22 @@ angular.module('myApp.register', ['ngRoute'])
 	});
 }])
 
-.controller('RegisterCtrl', [function() {
+.controller('RegisterCtrl', ['$scope', '$firebaseAuth', function($scope, $firebaseAuth) {
 
+	var firebaseObj = new Firebase("https://boiling-inferno-1527.firebaseio.com");
+	var auth = $firebaseAuth(firebaseObj);
+
+	$scope.signUp = function() {
+		if (!$scope.regForm.$invalid) {
+			var email = $scope.user.email;
+			var password = $scope.user.password;
+			if (email && password) {
+				auth.$createUser(email, password).then(function(){
+					console.log('User creation success');
+				}, function(error) {
+					console.log(error);
+				});
+			}
+		}
+	};
 }]);
