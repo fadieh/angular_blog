72d6439fe26f303e16aef4ca4332e221d398fd12
Fadie Hannona <fadiehannona@gmail.com>
2015-03-12 17:52:20 +0000

    karma

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,14 +1,16 @@
 module.exports = function(config){
   config.set({
 
-    basePath : './',
+    basePath : '../',
 
     files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-route/angular-route.js',
+      'app/bower_components/angular/angular.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/js/**/*.js',
+      'app/view*/**/*.js',
+      'test/unit/**/*.js'
     ],
 
     autoWatch : true,
